1080115010664f5be58c07f2766d7d4316b0df66
ElenaKorsakova <lenawwka@gmail.com>
2022-06-07 10:49:54 +0200

    'Fixed the format and simplified the function

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 function GetNumberIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
+
   if (max <= min || max < 0 || min < 0) {
     throw new Error('Entre the correct value range');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 GetNumberIntInclusive(-10, 20);
 // использовала https://developer.mozilla.org/ru/
 
 function checkCommentLength(comment, maxLength) {
-  if (comment.length <= maxLength) {
-    return true;
-  }
-  return false;
+  return comment.length <= maxLength;
 }
+
 checkCommentLength('Comment test', 140);
