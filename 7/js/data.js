530328a10ed4ae587c07d178381113eb0fbd4e11
Keks <keks@htmlacademy.ru>
2022-06-24 18:43:10 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/data.js
+++ 7/js/data.js
@@ -55,8 +55,8 @@ const LIKE_MIN = 15;
 const LIKE_MAX = 200;
 const AVATAR_MIN = 1;
 const AVATAR_MAX = 6;
-const NUMBERS_OF_PHOTO = 25;
-
+const NUMBER_OF_PHOTOS = 25;
+const NUMBER_OF_COMMENTS = 10;
 //функция-генератор комментариев
 function getComment() {
   const comment = {
@@ -65,12 +65,14 @@ function getComment() {
     message: MESSAGES[getRandomeNumber(0, MESSAGES.length)],
     name: NAMES[getRandomeNumber(0,NAMES.length)],
   };
-  const comments = [comment];
-  if (comment.id % 2) {
-    comments.push(comment);//создала возможность генерации либо 1 либо 2 комментов
+
+  const comments = [];
+
+  for (let i = 0; i <= NUMBER_OF_COMMENTS; i++) {
+    comments.push(comment);
   }
 
-  return comments;
+  return comments.slice(0, getRandomeNumber(0, NUMBER_OF_COMMENTS));
 }
 //функция - генератор фотографий
 function getNewPhoto(id) {
@@ -86,7 +88,7 @@ function getNewPhoto(id) {
 function createAllPhotos() {
   const allPhotos = [];
 
-  for (let i = 1; i <= NUMBERS_OF_PHOTO; i++) {
+  for (let i = 1; i <= NUMBER_OF_PHOTOS; i++) {
     allPhotos.push(getNewPhoto(i));
   }
 
--- 7/js/main.js
+++ 7/js/main.js
@@ -1,1 +1,10 @@
-import './photo-create.js';
+import {createAllPhotos} from './data.js';
+import {createUsersPhoto} from './photo-create.js';
+
+const usersPhotos = createAllPhotos();
+const pictureListFragment = document.createDocumentFragment();
+const picturesList = document.querySelector ('.pictures');
+
+usersPhotos.forEach((photo) => pictureListFragment.appendChild(createUsersPhoto(photo)));
+
+picturesList.appendChild(pictureListFragment);
