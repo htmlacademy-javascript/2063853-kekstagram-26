a895f34e097097324ee50c15ea7cadebce7710f4
ElenaKorsakova <lenawwka@gmail.com>
2022-06-27 15:43:54 +0200

    Connected thumbnails and full photos

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-import {createAllPhotos} from './data.js';
-import {createUsersPhoto} from './photo-create.js';
+import { createAllPhotos } from './data.js';
+import { createUsersPhoto } from './photo-create.js';
+import { openPhotoFull } from './photo-full.js';
+import { closePhotoFull } from './photo-full.js';
 
 const usersPhotos = createAllPhotos();
 const pictureListFragment = document.createDocumentFragment();
-const picturesList = document.querySelector ('.pictures');
+const picturesList = document.querySelector('.pictures');
+//на каждую миниатюру добавим обработчик события по клику и соберем их в фрагмент
+function addThumbnailClickHandler(photo) {
+  const thumbnail = createUsersPhoto(photo);
+  thumbnail.addEventListener('click', () => openPhotoFull(photo));
+  pictureListFragment.appendChild(thumbnail);
+}
 
-usersPhotos.forEach((photo) => pictureListFragment.appendChild(createUsersPhoto(photo)));
+usersPhotos.forEach(addThumbnailClickHandler);
 
 picturesList.appendChild(pictureListFragment);
