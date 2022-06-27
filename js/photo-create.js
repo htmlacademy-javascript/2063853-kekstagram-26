22180e61ea0c7f8d0700a9187076c0bdbefbc30b
ElenaKorsakova <lenawwka@gmail.com>
2022-06-27 12:41:59 +0200

    Used dot notation

--- js/photo-create.js
+++ js/photo-create.js
@@ -4,7 +4,7 @@ const photoTemplate = document.querySelector('#picture').content.querySelector('
 //функция - отрисовыватель фото по шаблону
 function createUsersPhoto ({url, like, comments}) {
   const photo = photoTemplate.cloneNode(true);
-  photo.querySelector('.picture__img').setAttribute('src', url);
+  photo.querySelector('.picture__img').src = url;
   photo.querySelector('.picture__comments').textContent = comments.length;
   photo.querySelector('.picture__likes').textContent = like;
   return photo;
