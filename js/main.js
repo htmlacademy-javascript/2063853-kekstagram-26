2dc0fc53aa4d3a477cbb05d02a8145e1fa81506f
ElenaKorsakova <lenawwka@gmail.com>
2022-07-10 17:13:17 +0200

    Заменила моковые данные с фото пользователей на данные с сервера

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
-import { createAllPhotos } from './data.js';
 import { createUsersPhoto } from './photo-create.js';
-import { openPhotoFull} from './photo-full.js';
+import { thumbnailClickHandler } from './photo-full.js';
 import './photo-input.js';
 import './photo-edit.js';
+import { getData } from './api.js';
 
-const usersPhotos = createAllPhotos();
 const pictureListFragment = document.createDocumentFragment();
 const picturesList = document.querySelector('.pictures');
 
-usersPhotos.forEach(addThumbnailClickHandler);
+function showUsersPhotos(photos) {
+  photos.forEach(addThumbnailClickHandler);
+  picturesList.appendChild(pictureListFragment);
+}
+
+getData(showUsersPhotos);
 
 //на каждую миниатюру добавим обработчик события по клику и соберем их в фрагмент
 function addThumbnailClickHandler(photo) {
   const thumbnail = createUsersPhoto(photo);
 
-  thumbnail.addEventListener('click', () => openPhotoFull(photo));
+  thumbnail.addEventListener('click', () => thumbnailClickHandler(photo));
 
   pictureListFragment.appendChild(thumbnail);
 }
-
-picturesList.appendChild(pictureListFragment);
